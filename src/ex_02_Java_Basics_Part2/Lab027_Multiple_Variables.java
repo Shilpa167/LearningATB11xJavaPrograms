9373a62fa7b64563e91bace2a6d210ca772c3cac
Shilpa167 <shendeshilpa16@gmail.com>
2025-04-09 01:35:42 +0530

    14th March- I'm pushing my codes

--- src/ex_02_Java_Basics_Part2/Lab027_Multiple_Variables.java
+++ src/ex_02_Java_Basics_Part2/Lab027_Multiple_Variables.java
@@ -4,5 +4,8 @@ public class Lab027_Multiple_Variables {
     public static void main(String[] args) {
 
         int a = 10, b = 23, c = 99;
+        int _ = 10;
+        //System.out.println(_);
+
     }
 }
